cccd57b3ca0430454ac8e30445bc78e1b0b4e2ab
Leonie Scheidweiler <leonie.olivia.scheidweiler@gmail.com>
2025-02-25 15:55:24 +0100

    restructured main matched filter function to split up the gathering of necessary variables. put settings into a dictionary

--- run/matched_filter.py
+++ run/matched_filter.py
@@ -7,52 +7,79 @@ import os
 
 def main():
     # matched filter method: linewise, whole_scene
-    matched_filter_method = "linewise"
+    settings = {
+        "uas_grid": "mean_amf",  # mean_amf, airmass
+        "method": "linewise",  # linewise, whole_scene
+        "iterative": True,  # bool
+    }
 
     root_data = xr.open_dataset("SYNTH_SPECTRA/L1B_DATA.nc")
 
     # run matched filter
     win = [[1975, 2095]]
-    alpha_co2 = matched_filter("co2", win, matched_filter_method)
+    alpha_co2 = matched_filter("co2", win, settings)
 
     win = [[2120, 2430]]
-    alpha_ch4 = matched_filter("ch4", win, matched_filter_method)
+    alpha_ch4 = matched_filter("ch4", win, settings)
 
-    # plt.xlabel("Longitude / degree")
-    # plt.ylabel("Latitude / degree")
-    # plt.pcolor(
-    #     root_data.longitude, root_data.latitude, alpha_co2,
-    #     cmap="inferno_r"
-    # )
-    # plt.colorbar(label="XCO2 enhancement / ppm")
-    # plt.show()
+    plt.xlabel("Longitude / degree")
+    plt.ylabel("Latitude / degree")
+    plt.pcolor(
+        root_data.longitude, root_data.latitude, alpha_co2,
+        cmap="inferno_r"
+    )
+    plt.colorbar(label="XCO2 enhancement / ppm")
+    plt.show()
 
-    # plt.xlabel("Longitude / degree")
-    # plt.ylabel("Latitude / degree")
-    # plt.pcolor(
-    #     root_data.longitude, root_data.latitude, alpha_ch4,
-    #     cmap="inferno_r"
-    # )
-    # plt.colorbar(label="XCH4 enhancement / ppm")
-    # plt.show()
+    plt.xlabel("Longitude / degree")
+    plt.ylabel("Latitude / degree")
+    plt.pcolor(
+        root_data.longitude, root_data.latitude, alpha_ch4,
+        cmap="inferno_r"
+    )
+    plt.colorbar(label="XCH4 enhancement / ppm")
+    plt.show()
 
     write_output(root_data, alpha_co2, alpha_ch4)
 
 
-def matched_filter(gas, win, matched_filter_method):
+def matched_filter(gas, win, settings):
     print(
-        f"running matched filter version \"{matched_filter_method}\" "
+        f"running matched filter with settings {settings} "
         f"for gas {gas} with {len(win)} fit windows with boundaries {win}."
     )
-    wl, x, s = get_variables(gas, win, matched_filter_method)
-    alpha, x, mu, t, cov, cov_inv \
-        = run_matched_filter(wl, x, s, matched_filter_method)
+
+    # get wavelength grid, radiances on the spatial grid, and the unit absorption spectrum
+    wl, x, s = get_variables(gas, win, settings)
+
+    # normalize radiances for numerical reasons. t (the target signature) will be normalized
+    # automatically, since it is calculated from mu*s, where mu is calculated from x.
+    norm = 1e11
+    x = x / norm
+
+    # calculate statistical properties mu (mean radiance) and cov (covariance matrix)
+    mu, cov, cov_inv = statistical_properties(x, settings)
+
+    # get target signature
+    t = mu * s
+
+    # run matched filter
+    alpha = run_matched_filter(x, mu, cov_inv, t)
+
     # output_debug_info(alpha, x, mu, t, cov, cov_inv)
 
     return alpha
 
 
-def get_variables(gas, win, matched_filter_method):
+def get_variables(gas, win, settings):
+    match settings["uas_grid"]:
+        case "mean_amf":
+            pass
+        case "airmass":
+            sys.exit("uas_grid airmass not implemented.")
+        case _:
+            sys.exit(f"uas_grid {settings['uas_grid']} not implemented.")
+
     rundir = os.getcwd()
     scriptdir = os.path.dirname(__file__)
 
@@ -83,129 +110,104 @@ def get_variables(gas, win, matched_filter_method):
     sza = np.deg2rad(root_data.solar_zenith_angle)
     vza = np.deg2rad(root_data.viewing_zenith_angle)
     mean_amf = 0.5 * (1/np.cos(sza) + 1/np.cos(vza))
-    match matched_filter_method:
+
+    match settings["method"]:
         case "linewise":
             mean_amf = mean_amf.mean(dim="frame")
             s = uas_data.uas.interp(amf=mean_amf)
             s = s.expand_dims(dim={"frame": Nframes})
             s = s.drop_vars("amf")
+
         case "whole_scene":
             mean_amf = mean_amf.mean(dim=("frame", "line"))
             s = uas_data.uas.interp(amf=mean_amf)
             s = s.expand_dims(dim={"frame": Nframes, "line": Nlines})
             s = s.drop_vars("amf")
+
+        case _:
+            sys.exit(f"method {settings['method']} not implemented.")
+
     # get s on the spectral grid of the measurement
     s = s.interp(wavelength=wl)
 
     return wl, x, s
 
 
-def run_matched_filter(wl, x, s, matched_filter_method):
-    match matched_filter_method:
-        case "linewise":
-            mf = mf_linewise
+def statistical_properties(x, settings):
+    match settings["method"]:
         case "whole_scene":
-            mf = mf_whole_scene
+            Nframes = x.sizes["frame"]
+            Nlines = x.sizes["line"]
 
-    alpha, x, mu, t, cov, cov_inv = mf(wl, x, s)
+            # for calculations, stack both spatial dimensions of x onto one
+            x = x.stack(pixel=("frame", "line"))
 
-    return alpha, x, mu, t, cov, cov_inv
+            # get mu on the same grid as x
+            mu = x.mean(dim="frame")
+            mu = mu.expand_dims(dim={"frame": Nframes})
+
+            # get cov on the same grid as x
+            # but with the dimension wavelength/wavelength
+            # also get cov_inv
+            cov = xr.apply_ufunc(
+                lambda x: np.cov(x, rowvar=False),
+                x,
+                input_core_dims=[["pixel", "wavelength"]],
+                output_core_dims=[["wavelength1", "wavelength2"]],
+                vectorize=True,
+            )
+            cov_inv = xr.apply_ufunc(
+                lambda x: np.linalg.pinv(x, rcond=1e-5),
+                cov,
+                input_core_dims=[["wavelength1", "wavelength2"]],
+                output_core_dims=[["wavelength1", "wavelength2"]],
+                vectorize=True,
+            )
+            cov = cov.expand_dims(dim={"frame": Nframes, "line": Nlines})
+            cov_inv = cov_inv.expand_dims(dim={"frame": Nframes, "line": Nlines})
+            print(f"condition number: {np.linalg.cond(cov[0, 0, :, :])}")
+
+        case "linewise":
+            Nframes = x.sizes["frame"]
+
+            # get mu on the same grid as x
+            mu = x.mean(dim="frame")
+            mu = mu.expand_dims(dim={"frame": Nframes})
+
+            # get cov on the same grid as x
+            # but with the dimension wavelength/wavelength
+            # also get cov_inv
+            cov = xr.apply_ufunc(
+                lambda x: np.cov(x, rowvar=False),
+                x,
+                input_core_dims=[["frame", "wavelength"]],
+                output_core_dims=[["wavelength1", "wavelength2"]],
+                vectorize=True,
+            )
+            cov_inv = xr.apply_ufunc(
+                lambda x: np.linalg.pinv(x, rcond=1e-5),
+                cov,
+                input_core_dims=[["wavelength1", "wavelength2"]],
+                output_core_dims=[["wavelength1", "wavelength2"]],
+                vectorize=True,
+            )
+            cov = cov.expand_dims(dim={"frame": Nframes})
+            cov_inv = cov_inv.expand_dims(dim={"frame": Nframes})
+            print(f"condition number: {np.linalg.cond(cov[0, 0, :, :])}")
+
+        case _:
+            sys.exit(f"method {settings['method']} not implemented.")
+
+    return mu, cov, cov_inv
 
 
-def mf_linewise(wl, x, s):
-    Nframes = x.sizes["frame"]
-
-    # normalize everything for easier numerical calculations
-    # normalizing x will also normalize mu and t and cov
-    # for alpha, this norm cancels out
-    # for alpha_err, it has to be corrected for
-    norm = 1e11
-    x = x / norm
-
-    # get mu on the same grid as x
-    mu = x.mean(dim="frame")
-    mu = mu.expand_dims(dim={"frame": Nframes})
-
-    # get t on the same grid as x
-    t = mu * s
-
-    # get cov on the same grid as x
-    # but with the dimension wavelength/wavelength
-    # also get cov_inv
-    cov = xr.apply_ufunc(
-        lambda x: np.cov(x, rowvar=False),
-        x,
-        input_core_dims=[["frame", "wavelength"]],
-        output_core_dims=[["wavelength1", "wavelength2"]],
-        vectorize=True,
-    )
-    cov_inv = xr.apply_ufunc(
-        lambda x: np.linalg.pinv(x, rcond=1e-5),
-        cov,
-        input_core_dims=[["wavelength1", "wavelength2"]],
-        output_core_dims=[["wavelength1", "wavelength2"]],
-        vectorize=True,
-    )
-    cov = cov.expand_dims(dim={"frame": Nframes})
-    cov_inv = cov_inv.expand_dims(dim={"frame": Nframes})
-    print(f"condition number: {np.linalg.cond(cov[0, 0, :, :])}")
-
+def run_matched_filter(x, mu, cov_inv, t):
     # calculate alpha on the same grid as x
     num = np.einsum("fli,flij,flj->fl", (x-mu), cov_inv, t)
     den = np.einsum("fli,flij,flj->fl", t, cov_inv, t)
     alpha = num/den
 
-    return alpha, x, mu, t, cov, cov_inv
-
-
-def mf_whole_scene(wl, x, s):
-    Nframes = x.sizes["frame"]
-    Nlines = x.sizes["line"]
-
-    # for calculations, stack both spatial dimensions of x onto one
-    x = x.stack(pixel=("frame", "line"))
-
-    # normalize everything for easier numerical calculations
-    # normalizing x will also normalize mu and t and cov
-    # for alpha, this norm cancels out
-    # for alpha_err, it has to be corrected for
-    norm = 1e11
-    x = x / norm
-
-    # get mu on the same grid as x
-    mu = x.mean(dim="pixel")
-    mu = mu.expand_dims(dim={"frame": Nframes, "line": Nlines})
-
-    # get t on the same grid as x
-    t = mu * s
-
-    # get cov on the same grid as x
-    # but with the dimension wavelength/wavelength
-    # also get cov_inv
-    cov = xr.apply_ufunc(
-        lambda x: np.cov(x, rowvar=False),
-        x,
-        input_core_dims=[["pixel", "wavelength"]],
-        output_core_dims=[["wavelength1", "wavelength2"]],
-        vectorize=True,
-    )
-    cov_inv = xr.apply_ufunc(
-        lambda x: np.linalg.pinv(x, rcond=1e-5),
-        cov,
-        input_core_dims=[["wavelength1", "wavelength2"]],
-        output_core_dims=[["wavelength1", "wavelength2"]],
-        vectorize=True,
-    )
-    cov = cov.expand_dims(dim={"frame": Nframes, "line": Nlines})
-    cov_inv = cov_inv.expand_dims(dim={"frame": Nframes, "line": Nlines})
-    print(f"condition number: {np.linalg.cond(cov[0, 0, :, :])}")
-
-    # calculate alpha on the same grid as x
-    num = np.einsum("fli,flij,flj->fl", (x-mu), cov_inv, t)
-    den = np.einsum("fli,flij,flj->fl", t, cov_inv, t)
-    alpha = num/den
-
-    return alpha, x, mu, t, cov, cov_inv
+    return alpha
 
 
 def output_debug_info(alpha, x, mu, t, cov, cov_inv):
